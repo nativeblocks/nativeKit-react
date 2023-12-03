feba75cb8f56834cb8db7efa2c68a03dc0ebb4a3
Alireza <alireza@nativeblocks.io>
2023-12-03 15:10:47 +0300

    init project

---
+++ blockKit/rollup.config.mjs
@@ -0,0 +1,43 @@
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
+import dts from "rollup-plugin-dts";
+import { terser } from "rollup-plugin-terser";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import graphql from "@apollo-elements/rollup-plugin-graphql";
+
+const packageJson = require("../package.json");
+
+export default [
+  {
+    input: "src/index.ts",
+    output: [
+      {
+        file: packageJson.main,
+        format: "cjs",
+        sourcemap: false,
+      },
+      {
+        file: packageJson.module,
+        format: "esm",
+        sourcemap: false,
+      },
+    ],
+    plugins: [
+      peerDepsExternal(),
+      resolve(),
+      commonjs(),
+      terser(),
+      graphql(),
+      typescript({
+        tsconfig: "./tsconfig.json",
+      }),
+    ],
+    external: ["react", "react-dom", "react-router-dom"],
+  },
+  {
+    input: "dist/esm/types/index.d.ts",
+    output: [{ file: "dist/types.d.ts", format: "esm" }],
+    plugins: [dts.default()],
+  },
+];
